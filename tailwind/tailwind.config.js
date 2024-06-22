7923d21424d6461f80222ecf4b7ba47985e53f2e
Abhinav Singh <sokhiabhinavsingh@gmail.com>
2024-06-22 21:14:37 +0530

    Set up and configuration of TW

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './tailwindapp/templates/**/*.html',
+    './tailwindapp/static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
